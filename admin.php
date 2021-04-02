72269acb8b1192b37bc490ea6fe6720536e8ea94
beqakhelidze <khelidzeb@gmail.com>
2021-04-03 00:36:02 +0400

    shecvla saxlshi

--- admin.php
+++ admin.php
@@ -1,4 +1,4 @@
-<!-- achia -->
+<!-- baqari -->
 
 <?php include './header.php';?>
 <nav class="navbar navbar-light" style="background-color: #1f5881; height: 50px;">
@@ -17,7 +17,7 @@
         <div class="dropdown">
             <button onclick="myFunction()" class="dropbtn"><span class="material-icons NavMenuSpan" onclick="myFunction()">more_vert</span></button>
             <div id="myDropdown" class="dropdown-content">
-                <a class="addCategoryBtn" href="#home"  data-bs-toggle="modal" data-bs-target="#showaddModal"><span class="material-icons logoutIcon">add</span>&nbsp;მეთოდები&nbsp;კატეგორიებზე</a>
+                <a class="addCategoryBtn" href="#home"  data-bs-toggle="modal" data-bs-target=".addCategoryModal" data-bs-whatever="@addCategory"><span class="material-icons logoutIcon">add</span>&nbsp;კატეგორიის დამატება</a>
                 <!-- <a href="#about" data-bs-toggle="modal" data-bs-target=".addProductModal" data-bs-whatever="@addProduct"><span class="material-icons logoutIcon">add</span>&nbsp;პროდუქტის დამატება</a> -->
                 <a href="#contact"><span class="material-icons logoutIcon">logout</span>&nbsp;გასვლა</a>
             </div>
@@ -50,7 +50,6 @@
 
 </div>
 
-<!-- კატეგორიის დამატების მოდალი -->
 <div class="modal fade scale-up-center addCategoryModal" id="showaddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
     <div class="modal-content">
@@ -58,11 +57,16 @@
         <h5 class="modal-title" id="exampleModalLabel">მეთოდები კატეგორიებზე</h5>
         <span class="material-icons" data-bs-dismiss="modal" aria-label="Close" style="color: white; cursor: pointer;">close</span>
       </div>
+      <!-- <form class="addCategory"> -->
       <div class="modal-body" >
         <div class="addDiv" style="display: flex; align-items: center;">
             
         </div>
       </div>
+      <!-- <div class="modal-footer">
+        <button type="submit" class="btn btn-primary SaveBtn" data-bs-dismiss="modal" id="liveToastBtn">შენახვა</button>
+      </div> -->
+      <!-- </form> -->
     </div>
   </div>
 </div>
@@ -122,7 +126,11 @@
                           
               </div>  
           </div>
-
+              <!-- <div class="upload-btn imgUpload" onclick="selectFile()">
+                  <i class="fas fa-cloud-upload-alt"></i>
+                  <span>Choose files to Upload</span>
+                  <input type="file" id="upload-file" onchange="uploads(event)" multiple="" accept="image/*">
+              </div> -->
             </div>
           </div>
       </div>
@@ -335,6 +343,7 @@ image.onload = function() {
 
         
     $('.Rest-images').eq(a).attr({'org-src':canvas.toDataURL()});
+    $('.BigRest-image').eq(a).attr({'src':canvas.toDataURL()});
     $('.mySlides').eq(a).attr({'src':canvas.toDataURL()});
     // return canvas.toDataURL();
 
@@ -345,6 +354,8 @@ image.onload = function() {
 var main_picture;
 
 //function uploads(event);
+
+
 
 const uploads = (event) => {
 
@@ -364,18 +375,24 @@ for (let i = 0; i < files.length; i++) {
         
         // images.innerHTML += `<div class="loadImage"></div>`
         var a = $('#images').children().length;
+
+        var date = Date.now();
+
         // setTimeout(function(){
         images.innerHTML += `<div class="container containerImg AddImgCount${a}" data-count="${a}" >
           <img class="Rest-images"  data-count="${a}" src="" org-src="">
           <div class="overlay"></div>
           <div class="input">
-            <span class="material-icons dish-icon showRestImg" data-count="${a}" onclick="openModal(${a})" data-toggle="tooltip" data-placement="bottom" title="დათვალიერება">visibility</span>
+            <span class="material-icons dish-icon showRestImg" data-count="${a}"  onclick="openModal(${date})")" data-toggle="tooltip" data-placement="bottom" title="დათვალიერება">visibility</span>
             <!-- <span class="material-icons dish-icon default-icon CheckImg CheckRestImg" data-count="${a}" main-img="0" data-toggle="tooltip" data-placement="bottom" title="მთავარი სურათი">inbox</span> -->
             <span class="material-icons dish-icon deleteImg deleteNewRestImg" data-toggle="modal" data-target=".modal-deleteImg" data-delete="${a}" data-toggle="tooltip" data-placement="bottom" title="წაშლა">delete</span>
           </div>
           <p class="main hidden otherfont">მთავარი</p>
-          <div id="id01" class="modal">
-              <img class="Rest-images" data-count = "${a}" src="'>
+          <div id="${date}" class="modal modalforPhoto" >
+              <span class="material-icons">
+                close
+              </span>
+              <img class="BigRest-image shadow" src="">
           </div>
         </div>`
 
@@ -386,9 +403,11 @@ for (let i = 0; i < files.length; i++) {
         document.querySelectorAll(".deleteImg").forEach(element => {
             element.addEventListener("click", () =>{
               element.parentElement.parentElement.remove();
-              changeMainElement();
+              ChangeMainElement();
             })
         });
+
+        
 
 
         // document.querySelectorAll(".CheckImg").forEach(element => {
@@ -452,8 +471,18 @@ function ChangeMainElement() {
 
 
 function openModal(e) {
-  console.log("kaia");
+  console.log(e);
+  document.getElementById(e).style.display = "block";
+
+  var modal = document.getElementById(e);
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
 }
+
 
 function closeModal() {
     $('#myModal').hide();
@@ -483,4 +512,8 @@ function showDivs(n) {
   x.eq(slideIndex-1).show();
 
 }
+
+
+
+
 </script>
